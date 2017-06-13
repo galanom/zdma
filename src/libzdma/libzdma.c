58a053ad89c2655ba9b199848850733c51c1049e
Ioannis Galanommatis <igalanommatis@isc.tuc.gr>
2017-06-13 23:04:56 +0300

    provisional 2

---
+++ src/libzdma/libzdma.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "zdma_ioctl.h"
+
+int main(int argc, char **argv)
+{
+	int fd, len = 1024;
+	if ((fd = open("/dev/zdma", O_RDWR|O_SYNC)) < 0) {
+		perror("open");
+		exit(-1);
+	}
+	ioctl(fd, ZDMA_IO_SET_DMA_SIZE, 1024<<16 | 1024);
+//	ioctl(fd, ZDMA_IO_SET_DMA_RX_SIZE, 512);
+/*	unsigned *kadr = mmap(0, len, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_LOCKED, fd, 0);
+	if (kadr == MAP_FAILED) {
+		perror("mmap");
+		exit(-1);
+	}
+	printf("ret=%x\n", kadr[0]);*/
+	return 0;
+}
---
+++ src/libzdma/zdma_ioctl.h
@@ -0,0 +1,1 @@
+../zdma/zdma_ioctl.h
